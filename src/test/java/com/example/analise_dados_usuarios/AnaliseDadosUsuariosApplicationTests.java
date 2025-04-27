8ee8475162dd36757f44c6e97729b74458d7e85c
Mateus <mateus.dias029@gmail.com>
2025-04-27 13:02:26 -0300

    initial project structure

---
+++ src/test/java/com/example/analise_dados_usuarios/AnaliseDadosUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.analise_dados_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnaliseDadosUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
